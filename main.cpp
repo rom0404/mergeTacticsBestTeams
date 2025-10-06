9c383a034864a0aa75fbe25bf32580c338da32d6
rom0404 <ramy.m2004@gmail.com>
2025-10-06 15:47:42 +0300

    cleaned up code+added most common troops

--- main.cpp
+++ main.cpp
@@ -3,18 +3,35 @@
 #include <string>
 #include <algorithm>
 #include <map>
+#include <unordered_map>
 using namespace std;
+map<string, int> bestTeamsTroop;
+const int twoWeight = 2;
+const int fourWeight = 4;
+map<string, int> traits = {
+    {"noble", 1},
+    {"jug", 2},
+    {"clan", 3},
+    {"ranger", 4},
+    {"goblin", 5},
+    {"assassin", 6},
+    {"blaster", 7},
+    {"brawler", 8},
+    {"undead", 9},
+    {"fire", 10},
+    {"mage", 11},
+    {"avenger", 12},
+    {"ace", 13},
+    {"electric", 14}};
 
-// Troop as a struct
-struct Troop {
-    std::string name;
+struct Troop
+{
+    string name;
     int trait1;
     int trait2;
 };
 
-
-vector<Troop> initalizeTroops() {
-    std::vector<Troop> troops = {
+vector<Troop> troops = {
 
     {"knight", 1, 2},
     {"archer", 3, 4},
@@ -42,65 +59,118 @@ vector<Troop> initalizeTroops() {
     {"sking", 9, 2},
     {"goldenknight", 6, 1},
     {"queen", 3, 12},
-    };
-
-    return troops;
-}
+};
 
 // Generate all possible teams of given size from a vector of troops
-std::vector<std::vector<Troop>> generateTeams(const std::vector<Troop>& troops, int teamSize) {
-    std::vector<std::vector<Troop>> teams;
+vector<vector<Troop>> generateTeams(const vector<Troop> &troops, int teamSize)
+{
+    vector<vector<Troop>> teams;
     int n = troops.size();
-    std::vector<bool> select(n, false);
-    std::fill(select.end() - teamSize, select.end(), true);
+    vector<bool> select(n, false);
+    fill(select.end() - teamSize, select.end(), true);
 
-    do {
-        std::vector<Troop> team;
-        for (int i = 0; i < n; ++i) {
-            if (select[i]) team.push_back(troops[i]);
+    do
+    {
+        vector<Troop> team;
+        for (int i = 0; i < n; ++i)
+        {
+            if (select[i])
+                team.push_back(troops[i]);
         }
         teams.push_back(team);
-    } while (std::next_permutation(select.begin(), select.end()));
+    } while (next_permutation(select.begin(), select.end()));
     return teams;
 }
 
 // Grade a team according to trait rules
-int gradeTeam(const std::vector<Troop>& team) {
-    std::map<int, int> traitCounts;
-    for (const Troop& t : team) {
+int gradeTeam(const vector<Troop> &team)
+{
+    map<int, int> traitCounts;
+    for (const Troop &t : team)
+    {
         traitCounts[t.trait1]++;
         traitCounts[t.trait2]++;
     }
-    //remove goblin trait
-    traitCounts[5] = 0;
     int score = 0;
-    
+
     for (int i = 1; i < 15; i++)
     {
-        if (traitCounts[i]==2 || traitCounts[i]==3)
+        if (traitCounts[i] == 2 || traitCounts[i] == 3)
         {
-            score += 2;
-        } else if (traitCounts[i] == 4 || traitCounts[i] == 5)
+
+            score += twoWeight;
+        }
+        else if (traitCounts[i] == 4 || traitCounts[i] == 5)
         {
-            score += 4;
+            score += fourWeight;
         }
     }
 
     return score;
 }
 
+bool cmp(pair<string, int> &a,
+         pair<string, int> &b)
+{
+    return a.second < b.second;
+}
+
+// Function to sort the map according
+// to value in a (key-value) pairs
+void sort(map<string, int> &M)
+{
+
+    // Declare vector of pairs
+    vector<pair<string, int>> A;
+
+    // Copy key-value pair from Map
+    // to vector of pairs
+    for (auto &it : M)
+    {
+        A.push_back(it);
+    }
+
+    // Sort using comparator function
+    sort(A.begin(), A.end(), cmp);
+
+    // Print the sorted value
+    for (auto &it : A)
+    {
+
+        cout << it.first << ' '
+             << it.second << endl;
+    }
+}
+
+void mostCommonTroops(vector<vector<Troop>> &teams)
+{
+    for (const auto &team : teams)
+    {
+        for (const auto &troop : team)
+        {
+            bestTeamsTroop[troop.name]++;
+        }
+    }
+
+    sort(bestTeamsTroop);
+}
+
 // Given all teams, return those with the best score
-std::vector<std::vector<Troop>> filterBestTeams(const std::vector<std::vector<Troop>>& teams) {
+vector<vector<Troop>> filterBestTeams(const vector<vector<Troop>> &teams)
+{
     int bestScore = -1;
-    std::vector<std::vector<Troop>> bestTeams;
-    for (const auto& team : teams) {
+    vector<vector<Troop>> bestTeams;
+    for (const auto &team : teams)
+    {
         int score = gradeTeam(team);
-        if (score > bestScore) {
+        if (score > bestScore)
+        {
             bestScore = score;
             bestTeams.clear();
             bestTeams.push_back(team);
         }
-        else if (score == bestScore) {
+        else if (score == bestScore)
+        {
             bestTeams.push_back(team);
         }
     }
@@ -108,40 +178,25 @@ std::vector<std::vector<Troop>> filterBestTeams(const std::vector<std::vector<Tr
 }
 
 // Example usage
-int main() {
-    vector<Troop> troops = initalizeTroops();
-
+int main()
+{
     int teamSize = 6;
 
     auto allTeams = generateTeams(troops, teamSize);
     auto bestTeams = filterBestTeams(allTeams);
 
-    std::cout << "Number of best teams: " << bestTeams.size() << std::endl;
-    for (const auto& team : bestTeams) {
-        std::cout << "Team: ";
-        for (const auto& troop : team) {
-            std::cout << troop.name << " ";
+    cout << "Number of best teams: " << bestTeams.size() << endl;
+    for (const auto &team : bestTeams)
+    {
+        cout << "Team: ";
+        for (const auto &troop : team)
+        {
+            cout << troop.name << " ";
         }
-        std::cout << "\nScore: " << gradeTeam(team) << std::endl;
+        cout << "\nScore: " << gradeTeam(team) << endl;
     }
+
+    cout << "\n\n";
+    mostCommonTroops(bestTeams);
     return 0;
 }
-
-//trait decoding
-
-/*
-1 noble
-2 jug
-3 clan
-4 ranger
-5 goblin
-6 assassin
-7 blaster
-8 brawler
-9 undead
-10 fire
-11 mage
-12 avenger
-13 ace
-14 electric
-*/
